9ac6633985322f998aaff03e63b0708059786fc6
Yuya Sato / 佐藤優矢 <yuyasatowork@gmail.com>
2025-05-31 08:08:09 +0900

    Create e2e.py

---
+++ e2etest/e2e.py
@@ -0,0 +1,18 @@
+from requests_html import HTMLSession
+
+class E2E(object):
+   """E2E Test for Web Including Javascript"""
+   def __init__(self, url):
+      self.url = url
+      session = HTMLSession()
+      r = session.get(self.url)
+      r.html.render()
+   def exists(tag):
+      if r.html.find(tag):
+         return True
+      else:
+         return False
+   def get_text(tag):
+      if not r.html.find(tag):
+         return None
+      return [t.text for t in r.html.find(tag)]
