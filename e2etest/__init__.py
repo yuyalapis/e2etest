d6dcf221c0ff95d6bcd70dc77eebfea0a1e1d053
Yuya Sato / 佐藤優矢 <yuyasatowork@gmail.com>
2025-06-01 16:03:21 +0900

    Update __init__.py

--- e2etest/__init__.py
+++ e2etest/__init__.py
@@ -1,5 +1,5 @@
 import subprocess
-subprocess.run(["pip", "installl", "requests_html"])
-subprocess.run(["pip", "installl", "lxml_html_clean"])
+subprocess.run(["pip", "install", "-q", "requests_html"])
+subprocess.run(["pip", "install", "-q", "lxml_html_clean"])
 
 from .e2e import E2E
